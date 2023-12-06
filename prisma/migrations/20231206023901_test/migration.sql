68ba00ee0fc9b0c72754df1e2df619f27844faa7
Sam <smrarote@gmail.com>
2023-12-06 08:32:19 +0530

    prisma migration typo fixed

---
+++ prisma/migrations/20231206023901_test/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[username]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[email]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[mobile]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `social` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "email" VARCHAR(100) NOT NULL,
+ADD COLUMN     "mobile" VARCHAR(20),
+ADD COLUMN     "password" VARCHAR(255) NULL,
+ADD COLUMN     "social" JSON NOT NULL,
+ADD COLUMN     "username" VARCHAR(100) NOT NULL,
+ADD COLUMN     "verified" BOOLEAN NOT NULL DEFAULT false;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_mobile_key" ON "users"("mobile");
