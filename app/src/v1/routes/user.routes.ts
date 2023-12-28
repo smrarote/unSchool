0ecd99029ea977ab6d8127f8cc6069d5afa33c87
Sam <smrarote@gmail.com>
2023-12-28 17:08:26 +0530

    removed exta code

--- app/src/v1/routes/user.routes.ts
+++ app/src/v1/routes/user.routes.ts
@@ -7,9 +7,6 @@ const router = Router();
 // Authentication
 router.route('/signin').post(userValidators.signIn, validationHandler, userController.signIn);
 router.route('/signup').post(userValidators.signUp, validationHandler, userController.signUp);
-router.route('/protected').get(reqValidation, (req, res) => {
-  return res.status(200).json({ msg: 'protectd validated', data: req.body.auth });
-});
 // activity
 
 export default router;
